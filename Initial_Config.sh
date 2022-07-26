abf6c527a3093fc13076ebd6d62bf7e571ebc58d
UTPPO <110046890+UTPPO@users.noreply.github.com>
2022-07-26 12:54:22 -0600

    Update Initial_Config.sh

--- Initial_Config.sh
+++ Initial_Config.sh
@@ -4,5 +4,5 @@ sudo apt install update -y
 sudo apt install upgrade -y
 sudo timedatectl set-timezone America/Denver
 
-mv ~/unlocktheppo/dailyupdate.sh /usr/local/sbin/dailyupdate.sh
+sudo mv ~/unlocktheppo/dailyupdate.sh /usr/local/sbin/dailyupdate.sh
 echo "done!"
