aaf1444c8d4d47bbc4448ca8c915c2c44e7f6546
UTPPO <110046890+UTPPO@users.noreply.github.com>
2022-07-26 19:37:15 -0600

    Update Initial_Croc_Config.sh

--- Initial_Croc_Config.sh
+++ Initial_Croc_Config.sh
@@ -5,7 +5,7 @@ sudo apt upgrade -y
 
 sudo timedatectl set-timezone America/Denver
 
-wget https://github.com/UTPPO/UnlockThePPO/blob/main/dailyupdate.sh
+wget https://raw.githubusercontent.com/UTPPO/UnlockThePPO/main/dailyupdate.sh
 chmod +x ~/dailyupdate.sh
 sudo mv ~/dailyupdate.sh /usr/local/sbin/dailyupdate.sh
 
