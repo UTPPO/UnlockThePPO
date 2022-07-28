a6786f463ee94d5b22b28b9bff5e3383dff06f6e
UTPPO <110046890+UTPPO@users.noreply.github.com>
2022-07-28 15:06:41 -0500

    Update Initial_Config.sh

--- Initial_Config.sh
+++ Initial_Config.sh
@@ -9,10 +9,6 @@ wget https://raw.githubusercontent.com/UTPPO/UnlockThePPO/main/dailyupdate.sh
 chmod +x ~/dailyupdate.sh
 sudo mv ~/dailyupdate.sh /usr/local/sbin/dailyupdate.sh
 
-wget https://raw.githubusercontent.com/UTPPO/UnlockThePPO/main/Incoming_Executables.sh
-chmod +x ~/Incoming_Executables.sh
-sudo mv ~/Incoming_Executables.sh /usr/local/sbin/Incoming_Executables.sh
-
 wget https://raw.githubusercontent.com/UTPPO/UnlockThePPO/main/crontab
 sudo mv ~/crontab /etc/crontab
 
