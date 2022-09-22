b51a361ffef5343850e230ca94beb5b0a255cd7f
UTPPO <110046890+UTPPO@users.noreply.github.com>
2022-09-22 07:13:54 -0600

    Update Initial_Config.sh

--- Initial_Config.sh
+++ Initial_Config.sh
@@ -6,12 +6,12 @@ sudo apt full-upgrade -y
 sudo timedatectl set-timezone America/Denver
 
 wget https://raw.githubusercontent.com/UTPPO/UnlockThePPO/main/dailyupdate.sh
-sudo chmod +x /home/$USER/dailyupdate.sh
-sudo mv /home/$USER/dailyupdate.sh /usr/local/sbin/dailyupdate.sh
+sudo chmod +x ./dailyupdate.sh
+sudo mv ./dailyupdate.sh /usr/local/sbin/dailyupdate.sh
 
 wget https://raw.githubusercontent.com/UTPPO/UnlockThePPO/main/crontab
-sudo chown root /home/$USER/crontab
-sudo chgrp root /home/$USER/crontab
-sudo mv /home/$USER/crontab /etc/crontab
+sudo chown root ./crontab
+sudo chgrp root ./crontab
+sudo mv ./crontab /etc/crontab
 
 echo "done!"
